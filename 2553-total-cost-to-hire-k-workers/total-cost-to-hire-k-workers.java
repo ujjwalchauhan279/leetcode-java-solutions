976cfc88955e1e19aa26cff60ee77980d44170e2
Ujjwal chauhan <ujjwalchauhan279@gmail.com>
2026-08-07 01:52:04 +0530

    Time: 57 ms (33.36%) | Memory: 72.9 MB (51.57%) - LeetSync

--- 2553-total-cost-to-hire-k-workers/total-cost-to-hire-k-workers.java
+++ 2553-total-cost-to-hire-k-workers/total-cost-to-hire-k-workers.java
@@ -11,21 +11,25 @@ class Solution {
             while(startPq.size() < candidates && i<=j) startPq.add(costs[i++]);
             while(endPq.size() < candidates && i<=j) endPq.add(costs[j--]);
 
-            if(startPq.size() == 0){
-                while(k != 0){
-                    cost += endPq.remove();
-                    k--;
-                }
-                break;
-            }
-            if(endPq.size() == 0){
-                while(k != 0){
-                    cost += startPq.remove();
-                    k--;
-                }
-                break;
-            }
-            if(startPq.peek() <= endPq.peek()){
+            // if(startPq.size() == 0){
+            //     while(k != 0){
+            //         cost += endPq.remove();
+            //         k--;
+            //     }
+            //     break;
+            // }
+            // if(endPq.size() == 0){
+            //     while(k != 0){
+            //         cost += startPq.remove();
+            //         k--;
+            //     }
+            //     break;
+            // }
+
+            int startTop = (startPq.size() > 0)? startPq.peek(): Integer.MAX_VALUE;
+            int endTop = (endPq.size() > 0)? endPq.peek(): Integer.MAX_VALUE;
+
+            if(startTop <= endTop){
                 cost += startPq.remove();
             }
             else{
