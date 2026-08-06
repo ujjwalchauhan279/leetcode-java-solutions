98ce67b2a6b99911933ed84683869b32a7db57df
Ujjwal chauhan <ujjwalchauhan279@gmail.com>
2026-08-07 01:46:34 +0530

    Time: 53 ms (67.47%) | Memory: 72.6 MB (84.75%) - LeetSync

---
+++ 2553-total-cost-to-hire-k-workers/total-cost-to-hire-k-workers.java
@@ -0,0 +1,40 @@
+class Solution {
+    public long totalCost(int[] costs, int k, int candidates) {
+        PriorityQueue<Integer> startPq = new PriorityQueue<>();
+        PriorityQueue<Integer> endPq = new PriorityQueue<>();
+
+        int n = costs.length;
+        long cost = 0;
+        int i = 0;
+        int j = n-1;
+        while(k != 0){
+            while(startPq.size() < candidates && i<=j) startPq.add(costs[i++]);
+            while(endPq.size() < candidates && i<=j) endPq.add(costs[j--]);
+
+            if(startPq.size() == 0){
+                while(k != 0){
+                    cost += endPq.remove();
+                    k--;
+                }
+                break;
+            }
+            if(endPq.size() == 0){
+                while(k != 0){
+                    cost += startPq.remove();
+                    k--;
+                }
+                break;
+            }
+            if(startPq.peek() <= endPq.peek()){
+                cost += startPq.remove();
+            }
+            else{
+                cost += endPq.remove();
+            }
+
+            k--;
+        }
+
+        return cost;
+    }
+}
