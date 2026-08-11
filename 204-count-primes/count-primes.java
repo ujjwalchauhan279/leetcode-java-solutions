82721f0f74819950e5d124b29ca2cc2c01d00198
Ujjwal chauhan <ujjwalchauhan279@gmail.com>
2026-08-11 14:55:08 +0530

    Time: 100 ms (65.75%) | Memory: 71.5 MB (10.35%) - LeetSync

--- 204-count-primes/count-primes.java
+++ 204-count-primes/count-primes.java
@@ -2,13 +2,17 @@ class Solution {
     public int countPrimes(int n) {
         int arr[] = new int[n+1];
         int count = 0;
-        for(int i=2; i<n; i++){
+        for(int i=2; i*i<n; i++){
             if(arr[i] == 0){
-                count++;
-                for(int j = 2*i; j<n; j+=i){
+                for(int j = i*i; j<n; j+=i){
                     arr[j]++;
                 }
             }
+        }
+
+        for(int i=2 ; i<n; i++){
+            if(arr[i] == 0) 
+                count++;
         }
 
         return count;
