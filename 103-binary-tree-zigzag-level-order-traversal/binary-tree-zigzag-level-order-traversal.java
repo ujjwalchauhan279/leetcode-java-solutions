64d1497f0f7ae597b171735830f968654e8aed2b
Ujjwal chauhan <ujjwalchauhan279@gmail.com>
2026-08-18 23:59:44 +0530

    Time: 1 ms (80.50%) | Memory: 43.8 MB (44.61%) - LeetSync

---
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -0,0 +1,55 @@
+class Pair {
+    TreeNode r;
+    int level;
+
+    Pair(TreeNode r, int level) {
+        this.r = r;
+        this.level = level;
+    }
+}
+
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> list = new ArrayList<>();
+        List<Integer> innerList = new ArrayList<>();
+        if (root == null)
+            return list;
+
+        Queue<Pair> q = new ArrayDeque<>();
+        q.add(new Pair(root, 1));
+        int currLevel = 1;
+
+        while (q.size() > 0) {
+            Pair front = q.remove();
+
+            // if level is same
+            if (currLevel == front.level) {
+                innerList.add(front.r.val);
+            }
+            /* if level changes then store innerlist in outerlist
+            and innerlist ko new list sa point kr diya, currlevel also increase*/
+            else {
+                if (currLevel % 2 == 0) {
+                    Collections.reverse(innerList);
+                }
+                list.add(innerList);
+                innerList = new ArrayList<>();
+                innerList.add(front.r.val);
+                currLevel++;
+            }
+
+            // queue mai left right child insert kr diya
+            if (front.r.left != null)
+                q.add(new Pair(front.r.left, front.level + 1));
+            if (front.r.right != null)
+                q.add(new Pair(front.r.right, front.level + 1));
+        }
+
+        if (currLevel % 2 == 0) {
+            Collections.reverse(innerList);
+        }
+        list.add(innerList);
+
+        return list;
+    }
+}
