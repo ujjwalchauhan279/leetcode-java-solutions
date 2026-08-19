8b2eb8f01cca938ce7edfa9c41285d295f112ee7
Ujjwal chauhan <ujjwalchauhan279@gmail.com>
2026-08-19 14:19:46 +0530

    Time: 1 ms (70.08%) | Memory: 43.4 MB (93.44%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -1,27 +1,17 @@
 class Solution {
-    public void helper(TreeNode root, int arr[], int level){
+    public void helper(TreeNode root, List<Integer> list, int level){
         if(root == null) return;
 
-        arr[level] = root.val;
+        if(level >= list.size()) list.add(root.val);
+        else list.set(level, root.val);
 
-        helper(root.left, arr, level+1);
-        helper(root.right, arr, level+1);
-    }
-    
-    public int levels(TreeNode root){
-        if(root == null) return 0;
-
-        return 1 + Math.max(levels(root.left), levels(root.right));
+        helper(root.left, list, level+1);
+        helper(root.right, list, level+1);
     }
     public List<Integer> rightSideView(TreeNode root) {
-        int l = levels(root);
-
         List<Integer> list = new ArrayList<>();
-        int arr[] = new int[l];
 
-        helper(root, arr, 0);
-
-        for(int ele: arr) list.add(ele);
+        helper(root, list, 0);
 
         return list;
     }
