da3e9beac1cf63f17d7581c622f77f104f40b586
Ujjwal chauhan <ujjwalchauhan279@gmail.com>
2026-08-21 01:25:33 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (93.98%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
@@ -1,24 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    public void preOrder(TreeNode root, ArrayList<TreeNode> list){
-        if(root == null) return;
-
-        list.add(root);
-        preOrder(root.left, list); 
-        preOrder(root.right, list);
-    }
     public void flatten(TreeNode root) {
         if(root == null) return;
-        ArrayList<TreeNode> list = new ArrayList<>();
 
-        preOrder(root, list);
-        list.get(0).left = null;
+        TreeNode leftTree = root.left;
+        TreeNode rightTree = root.right;
+        root.left = null;
+        root.right = null;
 
-        for(int i=1; i<list.size(); i++){
-            list.get(i-1).right = list.get(i);
-            list.get(i).left = null;
-        }
+        flatten(leftTree);
+        flatten(rightTree);
 
-        list.get(list.size()-1).right = null;
+        root.right = leftTree;
 
+        TreeNode temp = root;
+
+        while(temp.right != null) temp = temp.right;
+
+        temp.right = rightTree;
     }
 }
