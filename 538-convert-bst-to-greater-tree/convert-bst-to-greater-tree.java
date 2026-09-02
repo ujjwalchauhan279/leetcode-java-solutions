a97e3a6a3bb4dd934beb4d0c8e0e789d8f61283e
Ujjwal chauhan <ujjwalchauhan279@gmail.com>
2026-09-02 15:50:20 +0530

    Time: 0 ms (100.00%) | Memory: 47.6 MB (23.27%) - LeetSync

--- 538-convert-bst-to-greater-tree/convert-bst-to-greater-tree.java
+++ 538-convert-bst-to-greater-tree/convert-bst-to-greater-tree.java
@@ -1,30 +1,16 @@
 class Solution {
-    static int i;
     static int sum;
-    public void inorder(TreeNode root, ArrayList<Integer> arr){
+    public void inorder(TreeNode root){
         if(root == null) return;
 
-        inorder(root.right, arr);
-        arr.add(root.val);
-        inorder(root.left, arr);
-    }
-    public void modify(TreeNode root, ArrayList<Integer> arr){
-        if(root == null) return;
-
-        modify(root.right, arr);
-        sum += arr.get(i++);
+        inorder(root.right);
+        sum += root.val;
         root.val = sum;
-        modify(root.left, arr);
+        inorder(root.left);
     }
     public TreeNode convertBST(TreeNode root) {
-        ArrayList<Integer> arr = new ArrayList<>();
-
-        inorder(root, arr);
-
         sum = 0;
-
-        i=0;
-        modify(root, arr);
+        inorder(root);
 
         return root;
     }
