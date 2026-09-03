6d39364681888f9170c5d5366b5f431838d54f7e
Ujjwal chauhan <ujjwalchauhan279@gmail.com>
2026-09-04 01:02:45 +0530

    Time: 1 ms (100.00%) | Memory: 46.6 MB (29.04%) - LeetSync

--- 99-recover-binary-search-tree/recover-binary-search-tree.java
+++ 99-recover-binary-search-tree/recover-binary-search-tree.java
@@ -1,27 +1,26 @@
 class Solution {
-    static int i;
-    public void inorder(TreeNode root, ArrayList<Integer> arr){
+    TreeNode first = null;
+    TreeNode second = null;
+    TreeNode prev = null;
+    public void inorder(TreeNode root){
         if(root == null) return;
 
-        inorder(root.left, arr);
-        arr.add(root.val);
-        inorder(root.right, arr);
-    }
-    public void modify(TreeNode root, ArrayList<Integer> arr){
-        if(root == null) return;
+        inorder(root.left);
+        if(prev != null && prev.val > root.val){
+            if(first == null) first = prev;
+            second = root;
+        }
 
-        modify(root.left, arr);
-        if(root.val != arr.get(i)) root.val = arr.get(i);
-        i++;
-        modify(root.right, arr);
+        prev = root;
+
+        inorder(root.right);
     }
     public void recoverTree(TreeNode root) {
-        ArrayList<Integer> arr = new ArrayList<>();
+        inorder(root);
 
-        inorder(root, arr);
-        Collections.sort(arr);
-
-        i = 0;
-        modify(root, arr);
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+        
     }
 }
