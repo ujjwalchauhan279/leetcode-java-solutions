7e79b1c7a21db323a6f728af9e7a4a391080c802
Ujjwal chauhan <ujjwalchauhan279@gmail.com>
2026-09-08 23:25:54 +0530

    Time: 15 ms (93.37%) | Memory: 48.6 MB (65.77%) - LeetSync

--- 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.java
+++ 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.java
@@ -5,14 +5,34 @@ class Solution {
         arr.add(root.val);
         inorder(root.right, arr);
     }
+    public List<Integer> merge(ArrayList<Integer> arr1, ArrayList<Integer> arr2){
+        ArrayList<Integer> result = new ArrayList<>();
+        int i=0; 
+        int j=0;
+        while(i<arr1.size() && j<arr2.size()){
+            int a = arr1.get(i);
+            int b = arr2.get(j);
+            if(a <= b){
+                result.add(a);
+                i++;
+            }
+            else{
+                result.add(b);
+                j++;
+            }
+        }
+        while(i<arr1.size()) result.add(arr1.get(i++));
+        while(j<arr2.size()) result.add(arr2.get(j++));
+
+        return result;
+    }
     public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
-        ArrayList<Integer> arr = new ArrayList<>();
+        ArrayList<Integer> arr1 = new ArrayList<>();
+        ArrayList<Integer> arr2 = new ArrayList<>();
 
-        inorder(root1, arr);
-        inorder(root2, arr);
+        inorder(root1, arr1);
+        inorder(root2, arr2);
 
-        Collections.sort(arr);
-
-        return arr;
+        return merge(arr1, arr2);
     }
 }
