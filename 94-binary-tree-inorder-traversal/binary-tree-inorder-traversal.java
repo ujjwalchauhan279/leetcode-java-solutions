54fd50a3d91c365ffe865a42f75bc7a17f8905db
Ujjwal chauhan <ujjwalchauhan279@gmail.com>
2026-09-08 22:21:57 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (32.66%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -1,24 +1,19 @@
-//Morris Traversal
-
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         ArrayList<Integer> list = new ArrayList<>();
+
         TreeNode curr = root;
         while(curr != null){
             if(curr.left != null){
-                //find predecessor
-                TreeNode p = curr;
-                p = curr.left;
-                while(p.right != null && p.right != curr) p = p.right;
+                TreeNode pred = curr.left;
+                while(pred.right != null && pred.right != curr) pred = pred.right;
 
-                //linking
-                if(p.right == null){
-                    p.right = curr;
+                if(pred.right == null){
+                    pred.right = curr;
                     curr = curr.left;
                 }
-                //unlinking
                 else{
-                    p.right = null;
+                    pred.right = null;
                     list.add(curr.val);
                     curr = curr.right;
                 }
