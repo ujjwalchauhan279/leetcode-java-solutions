c7a82c74d77f4dbc83b5d73f12f6a2c1a5c153c3
Ujjwal chauhan <ujjwalchauhan279@gmail.com>
2026-09-13 00:10:02 +0530

    Time: 19 ms (82.06%) | Memory: 46 MB (20.90%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,28 +1,37 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         int n = nums.length;
-        HashSet<List<Integer>> ansSet = new HashSet<>();
-        HashSet<Long> set = new HashSet<>();
         List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        int k = 0;
+        int l = 0;
 
-        for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                for(int k=j+1; k<n; k++){
-                    long sum = (long)nums[i] + nums[j] + nums[k];
-                    long rem = (long)target - sum;
-                    if(set.contains(rem)){
-                        List<Integer> innerList = new ArrayList<>(List.of(nums[i], nums[j], nums[k], (int)rem));
-                        Collections.sort(innerList);
-                        ansSet.add(innerList);
-                    }
-                    set.add((long)nums[k]);
+        for (int i = 0; i < n - 3; i++) {
+            while (i != 0 && i < n && nums[i] == nums[i - 1])
+                i++;
+            for (int j = i + 1; j < n - 2; j++) {
+                while (j != i+1 && j < n && nums[j] == nums[j - 1])
+                    j++;
+
+                k = j + 1;
+                l = n - 1;
+
+                while (k < l) {
+                    long sum = (long) nums[i] + nums[j] + nums[k] + nums[l];
+                    if (sum == target) {
+                        list.add(new ArrayList<>(List.of(nums[i], nums[j], nums[k], nums[l])));
+                        k++;
+                        l--;
+                        while (k < l && nums[k] == nums[k - 1])
+                            k++;
+                        while (k < l && nums[l] == nums[l + 1])
+                            l--;
+                    } else if (sum < target)
+                        k++;
+                    else
+                        l--;
                 }
-                set.clear();
             }
-        }
-
-        for(List<Integer> li: ansSet){
-            list.add(li);
         }
 
         return list;
